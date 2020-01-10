c1f7a9c5e7f6f5489fdb0d9dc49428fbe86b2ffd
Daniel Biagi <danielbiagi@gmail.com>
2020-01-10 08:22:40 -0300

    switching weather API

--- part2/countries/src/Details.js
+++ part2/countries/src/Details.js
@@ -1,47 +1,55 @@
-import React, { useState } from 'react'
-import Weather from './Weather'
-import axios from 'axios'
+import React, { useState } from "react";
+import Weather from "./Weather";
+import axios from "axios";
 
-const Details = (props) => {
-    const [oneTime, setOneTime] = useState(false)
-    const [weather, setWeather] = useState([])
-    const [country, setCountry] = useState('')
-    const getWeather = () => {
-        const city = props.country.capital
-        const url = "http://api.apixu.com/v1/current.json?key=d4f7b4f8ecbc4f7aa39223619192307&q=" + city
-        axios
-            .get(url)
-            .then(response => {
-                setWeather(response.data.current)
-                setOneTime(false)
-                setCountry(props.country.name)
-                return weather
-            })
+const Details = props => {
+  const [oneTime, setOneTime] = useState(false);
+  const [weather, setWeather] = useState([]);
+  const [country, setCountry] = useState("");
+  const getWeather = () => {
+    const city = props.country.capital;
+    const url =
+      "http://api.weatherstack.com/current?access_key=38ba5619a240d6afa46a64379b3e2362&units=m&query=" +
+      city;
+    console.log("url", url);
+    axios.get(url).then(response => {
+      console.log("response", response);
+      setWeather(response.data.current);
+      setOneTime(false);
+      setCountry(props.country.name);
+      return weather;
+    });
+  };
+
+  if (props.country.name) {
+    if (props.country.name !== country) {
+      if (!oneTime) {
+        getWeather();
+        setOneTime(true);
+      }
     }
 
-    if (props.country.name) {
-        if (props.country.name !== country) {
-            if (!oneTime) {
-                getWeather()
-                setOneTime(true)
-            }
-        }
+    return (
+      <div>
+        <h1>{props.country.name}</h1>
+        capital {props.country.capital}
+        <br />
+        population {props.country.population}
+        <br />
+        <h2>languages</h2>
+        {props.country.languages &&
+          props.country.languages.map(el => {
+            return (
+              <React.Fragment key={el.name}>
+                <li>{el.name}</li>
+              </React.Fragment>
+            );
+          })}
+        <img src={props.country.flag} alt="" width="200px" />
+        <Weather weather={weather} capital={props.country.capital} />
+      </div>
+    );
+  } else return <> </>;
+};
 
-        return (
-            <div>
-                <h1>{props.country.name}</h1>
-                capital {props.country.capital}<br />
-                population {props.country.population}<br />
-                <h2>languages</h2>
-                {props.country.languages && props.country.languages.map(el => {
-                    return <React.Fragment key={el.name}><li>{el.name}</li></React.Fragment>
-                })}
-                <img src={props.country.flag} alt="" width="200px" />
-                <Weather weather={weather} capital={props.country.capital} />
-            </div>
-        )
-    } else return (<> </>)
-}
-
-
-export default Details
+export default Details;
--- part2/countries/src/Weather.js
+++ part2/countries/src/Weather.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Weather = (props) => {
-    if (props.weather.condition !== undefined) {
-        const { temp_c, condition, wind_kph, wind_dir } = props.weather
-        const { icon } = condition
-        return (
-            <div>
-                <h3>{props.capital}</h3>
-                <b>temperature: </b> {temp_c} º C<br />
-                <img src={icon} alt="" /> <br />
-                <b>wind: </b> {wind_kph} kph direction {wind_dir} <br />
-            </div>
-        )
-    } else
-        return (
-            <div>
-                <p>Loading...</p>
-            </div>
-        )
-}
+const Weather = props => {
+  console.log("weather props", props);
+  if (props.weather !== undefined) {
+    const { temperature, wind_speed, wind_dir, weather_icons } = props.weather;
+    let icon = weather_icons ? weather_icons : null;
+    return (
+      <div>
+        <h3>{props.capital}</h3>
+        <b>temperature: </b> {temperature} º C<br />
+        <img src={icon} alt="" /> <br />
+        <b>wind: </b> {wind_speed} kph direction {wind_dir} <br />
+      </div>
+    );
+  } else
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+};
 
-
-export default Weather
+export default Weather;
